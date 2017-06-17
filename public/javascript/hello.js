1fd4e0cbc14126155ca5eaf79adc9f768c813190
Le Dinh Minh Long <jason@Les-MacBook-Air.local>
2017-06-17 21:54:13 +0930

    changing a bit in hello.js

--- public/javascript/hello.js
+++ public/javascript/hello.js
@@ -1,4 +1,11 @@
+//so in the javascript files, you define the function, those will  be
+//your script. And in your HTML, you will activate the function using
+//tags attributes
+
+//you can add several javascript files to your html frame,and use it like 
+//your own library
 function myFunction()
 {
-	document.getElementById("current_time").innerHTML = "hello"
+	document.getElementById("current_time").innerHTML = "Now this javascript is running"
 }
+
